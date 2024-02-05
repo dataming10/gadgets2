2ff08dc38523074a07a3e7d5ed7487598a0d9b8f
dataming10 <155842299+dataming10@users.noreply.github.com>
2024-02-06 06:46:19 +0800

    Update config.php

--- crud-practice/includes/config.php
+++ crud-practice/includes/config.php
@@ -1,12 +1,37 @@
 <?php
+class DatabaseConnection {
+    private $conn;
+
+    public function __construct($servername, $username, $password, $dbname) {
+        $this->conn = new mysqli($servername, $username, $password, $dbname);
+
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+    }
+
+    public function getConnection() {
+        return $this->conn;
+    }
+
+    public function closeConnection() {
+        if ($this->conn) {
+            $this->conn->close();
+        }
+    }
+}
+
+// Usage example
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "crud";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$dbConnection = new DatabaseConnection($servername, $username, $password, $dbname);
+$conn = $dbConnection->getConnection();
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+// ... Your other code ...
+
+// Close the connection when done
+$dbConnection->closeConnection();
 ?>
