04496c83e5ea9e87d19b4c1a9ebd668ee582d0c1
dataming10 <155842299+dataming10@users.noreply.github.com>
2024-02-06 07:18:48 +0800

    Update delete_item.php

--- crud-practice/delete_item.php
+++ crud-practice/delete_item.php
@@ -1,21 +1,33 @@
 <?php
 include('includes/config.php');
 
+class ItemDeleter {
+    private $conn;
+
+    public function __construct($conn) {
+        $this->conn = $conn;
+    }
+
+    public function deleteItem($id) {
+        $deleteItemSql = "DELETE FROM items WHERE id = ?";
+        $stmt = $this->conn->prepare($deleteItemSql);
+        $stmt->bind_param("i", $id);
+
+        if ($stmt->execute()) {
+            $success = "Item deleted successfully.";
+        } else {
+            $error = "Error deleting item: " . $stmt->error;
+        }
+
+        $stmt->close();
+    }
+}
+
 if (isset($_GET['id']) && is_numeric($_GET['id'])) {
     $id = $_GET['id'];
 
-    // Use prepared statements to delete the item
-    $delete_item_sql = "DELETE FROM items WHERE id = ?";
-    $stmt = $conn->prepare($delete_item_sql);
-    $stmt->bind_param("i", $id);
-
-    if ($stmt->execute()) {
-        $success = "Item deleted successfully.";
-    } else {
-        $error = "Error deleting item: " . $stmt->error;
-    }
-
-    $stmt->close();
+    $itemDeleter = new ItemDeleter($conn);
+    $itemDeleter->deleteItem($id);
 } else {
     $error = "Invalid item ID.";
 }
